1c9616744f2c2b206ba4cf53f2934cce5bdab970
James Counts <fritzjob18@nethere.com>
2012-08-28 11:27:03 -0700

    Unicast Delegate Example

---
+++ EventReflection.Demo/EventReflection.Demo/DelegateDemo.cs
@@ -0,0 +1,38 @@
+﻿namespace EventReflection.Demo
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+
+    using ApprovalTests;
+    using ApprovalTests.Reporters;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    [UseReporter(typeof(DiffReporter))]
+    public class DelegateDemo
+    {
+        [TestMethod]
+        public void GetInvocationList()
+        {
+            Func<bool> truth = Domain.AlwaysTrue;
+            IEnumerable<MethodInfo> methods = truth.GetInvocationList().Select(d => d.Method);
+            Approvals.VerifyAll(methods, string.Empty);
+        }
+
+        public class Domain
+        {
+            public static bool AlwaysTrue()
+            {
+                return true;
+            }
+
+            public static bool NeverFalse()
+            {
+                return true;
+            }
+        }
+    }
+}
